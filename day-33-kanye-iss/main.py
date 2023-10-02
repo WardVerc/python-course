76d5887b3a4fc222ea71b364950aa780238216c1
Ward Vercruyssen <ward.vercruyssen@studiohyperdrive.be>
2023-10-02 16:11:20 +0200

    d34 - iss + start quizler

--- day-33-kanye-iss/main.py
+++ day-33-kanye-iss/main.py
@@ -1,26 +1,43 @@
-from tkinter import *
 import requests
+from datetime import datetime
 
-def get_quote():
-    response = requests.get(url="https://api.kanye.rest")
+MY_LAT = 51.054340 # Your latitude
+MY_LONG = 3.717424 # Your longitude
+
+def is_iss_overhead(): 
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
-    canvas.itemconfig(quote_text, text=response.json()["quote"])
+    data = response.json()
 
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+    #Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
+    else:
+        return False
 
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
+def is_nighttime():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
 
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-get_quote()
+    time_now = datetime.now()
 
-window.mainloop()
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+    else:
+        return False
+
+if is_iss_overhead() and is_nighttime():
+    # send mail to yourself
+    print("Look up! ISS is cruisin' by Ghent.")
