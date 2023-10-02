ab502fd10dab2b91c0df3311e33193a8cc207e84
Ward Vercruyssen <ward.vercruyssen@studiohyperdrive.be>
2023-10-02 11:40:28 +0200

    30-31 password manager update, flashcard project

--- day-29-30-password-manager/main.py
+++ day-29-30-password-manager/main.py
@@ -22,6 +22,24 @@ def generate_password():
 
     pyperclip.copy(password)
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+def find_password():
+    website = website_entry.get()
+    try: 
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="Data file not found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details found for {website}.")
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
@@ -87,8 +105,8 @@ password_label = Label(text="Password")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 email_entry = Entry(width=35)
 email_entry.insert(0, "ward@gmail.com")
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -96,6 +114,8 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
