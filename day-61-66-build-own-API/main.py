17cfaf6c036b7a55d3344c0a958e6f440f126cf5
Ward Vercruyssen <ward.vercruyssen@studiohyperdrive.be>
2023-10-12 15:23:15 +0200

    day 66 - build own Python API with documentation

--- day-61-66-build-own-API/main.py
+++ day-61-66-build-own-API/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 '''
 Install the required packages first: 
@@ -36,6 +37,21 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    # Makes it easier to return data in the API
+    def to_dict(self):
+        #Method 1. 
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            #Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+        
+        #Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 with app.app_context():
     db.create_all()
@@ -47,12 +63,103 @@ def home():
     
 
 ## HTTP GET - Read Record
+@app.route("/random", methods=["GET"])
+def get_random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+
+    # see to_dict method in the db Cafe class:
+    return jsonify(cafe=random_cafe.to_dict())
+    # This does the same without the to_dict method:
+    # return jsonify(cafe={
+    #     "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+    #     "seats": random_cafe.seats,
+    #     "has_toilet": random_cafe.has_toilet,
+    #     "has_wifi": random_cafe.has_wifi,
+    #     "has_sockets": random_cafe.has_sockets,
+    #     "can_take_calls": random_cafe.can_take_calls,
+    #     "coffee_price": random_cafe.coffee_price,
+    # })
+
+@app.route("/all", methods=["GET"])
+def get_all_cafes():
+    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
+    all_cafes = result.scalars().all()
+    # # Method 1
+    # all_cafes_in_dict = []
+    # for cafe in all_cafes:
+    #     all_cafes_in_dict.append(cafe.to_dict())
+    # return jsonify(cafes=all_cafes_in_dict)
+    # Method 2
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
+
+@app.route("/search", methods=["GET"])
+def search_cafe():
+    name = request.args.get("loc")
+    result = db.session.execute(db.select(Cafe).where(Cafe.location == name.capitalize()))
+    all_result_cafes = result.scalars().all()
+    if len(all_result_cafes) == 0:
+        return jsonify(error={
+            "Not Found": "Sorry, we don't have a cafe at that location."
+            }
+            ), 404
+    else:
+        return jsonify(cafes=[cafe.to_dict() for cafe in all_result_cafes])
 
 ## HTTP POST - Create Record
+@app.route("/add", methods=["POST"])
+def add_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    print(new_cafe.name, new_cafe.has_wifi, new_cafe.seats)
+    return jsonify(response={"success": "Successfully added the new cafe."})
 
 ## HTTP PUT/PATCH - Update Record
+#PUT: swap the whole object with a new one
+#PATCH: only update one (or more?) property of an object
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def update_price(cafe_id: str):
+    new_price = request.args.get("new_price")
+    cafe = db.get_or_404(Cafe, cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."}), 200
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
 
 ## HTTP DELETE - Delete Record
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id: str):
+    api_key = request.args.get("api-key")
+    if api_key == "TopSecretAPIKey":
+        cafe = db.get_or_404(Cafe, cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted the cafe."}), 200
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+    else:
+        return jsonify(error={"Not Found": "Sorry, that's not allowed. Make sure you have the correct api key."}), 403
 
 
 if __name__ == '__main__':
